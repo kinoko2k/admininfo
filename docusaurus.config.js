89d518f235b03bd3f784787248451c20b8731441
Kinoko_2K <65443692+kinoko2k@users.noreply.github.com>
2023-08-11 16:21:52 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://kinoko2k.github.io/admininfo',
+  url: 'https://kinoko2k.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'admininfo',
+  baseUrl: '/admininfo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
